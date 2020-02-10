510a63e470c18bd8b56448fb9511a512a25e2d58
abraham.carranza <abraham.carranza@utags.edu.mx>
2020-02-10 09:20:20 -0600

    Componente de registro, carpetas de modelos  y de servicios agregados al proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TestComponent } from './components/test/test.component';
 import { RetroalimentacionComponent } from './components/retroalimentacion/retroalimentacion.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 
 const routes: Routes = [
+  {path: 'registro', component: RegistroComponent},
   {path: 'test', component: TestComponent},
   {path: 'retroalimentacion', component: RetroalimentacionComponent},
-  { path: '**', pathMatch: 'full', redirectTo: 'test' }
+  { path: '**', pathMatch: 'full', redirectTo: 'registro' }
 ];
 
 @NgModule({
