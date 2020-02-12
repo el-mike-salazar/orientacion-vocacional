f20ec7443c956d7194ddfa4716ddae56e25d697a
Francisco <francisco.tapia@utags.edu.mx>
2020-02-12 08:50:07 -0600

    Componente Indicaciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { TestComponent } from './components/test/test.component';
 import { RetroalimentacionComponent } from './components/retroalimentacion/retroalimentacion.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { IndicacionComponent } from './components/indicacion/indicacion.component';
 
 
 const routes: Routes = [
   {path: 'registro', component: RegistroComponent},
+  {path: 'indicacion', component: IndicacionComponent},
   {path: 'test', component: TestComponent},
   {path: 'retroalimentacion', component: RetroalimentacionComponent},
   { path: '**', pathMatch: 'full', redirectTo: 'registro' }
